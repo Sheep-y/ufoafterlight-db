4bd8b17b5e9d6feabf98230fd4a6983fe9e3e613
Sheep-y <your.sheepy@gmail.com>
2014-11-13 19:17:22 +0800

    Data for races, subraces, and senses (not included yet)

---
+++ data/txt_subrace.js
@@ -0,0 +1,8 @@
+ufoal.txt.subrace = [, /* Manually determined, not game data */
+"Man","Woman",
+"Reticulan Rebel","Reticulan Expedition",
+"Beastman Soldier","Beastman Mystic","Matriarch",
+"Walking Robot","Levitating Robot","Wheeled Robot","Spider Robot","Mech Robot",
+"Martian","Worm","Spider","Roller","Boss",
+"Natural Element","Plant Pet","Fireball"
+];
