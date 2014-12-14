90cec36d476dbabf4ba1b3fd1766badfb94a2dff
Sheep-y <your.sheepy@gmail.com>
2014-12-14 15:58:24 +0800

    Hide internal name for unused data.
    Unused data has no names and the "internal name" is just same as id.

--- res/desc.js
+++ res/desc.js
@@ -4,7 +4,8 @@ var txt = ns.txt, br = '<br/>', hr = '<hr/>', sp = ' &nbsp; &nbsp; ';
 
 /** Return the description of any entity */
 ns.get_desc = function ufoal_get_desc( e ) {
-   var content = 'Id: ' + e.id + ', ' + e.name;
+   var content = 'Id: ' + e.id;
+   if ( +e.name === e.Id ) content += ', ' + e.name;
    if ( e.unknown ) 
       content += br + '<b>This entry is unused in the final game</b>';
    content += hr;
