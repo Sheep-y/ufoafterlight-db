fc5ca053b5c6f630093f2cad84e92fee30b1090f
Sheep-y <your.sheepy@gmail.com>
2015-01-31 09:59:43 +0800

    Updated comparison description formats.

--- res/desc.js
+++ res/desc.js
@@ -1,11 +1,16 @@
 (function ufoal_desc( ns ){ 'use strict';
 
-var txt = ns.txt, br = '<br/>', hr = '<hr/>', sp = ' &nbsp; &nbsp; ';
+var txt = ns.txt, ui, br = '<br/>', hr = '<hr/>', sp = ' &nbsp; &nbsp; ';
 
 /** Return the description of any entity */
 ns.get_desc = function ufoal_get_desc( e ) {
    var method = 'get_' + e.type + '_desc', content = '';
-   if ( ! ns.ui.comparing ) {
+   if ( ! ui ) ui = ns.ui;
+   if ( ui.comparing ) {
+      if ( e.day ) content += e.day + ' days' + br;
+      else if ( e.hour ) content += e.hour + ' hours' + br;
+      else content += e.name + br;
+   } else {
       content += 'Id: ' + e.id;
       if ( +e.name !== e.id ) content += ', ' + e.name;
       if ( e.unknown )
@@ -29,14 +34,14 @@ ns.get_hint = function ufoal_get_hint( e ) {
 }
 
 ns.get_building_desc = function ufoal_get_building_desc( e ) {
-   var result = ns.ui.comparing ? '' : ns.get_general_desc( e ) + hr, people = [];
+   var result = ui.comparing ? '' : ns.get_general_desc( e ) + hr, people = [];
    result += ( e.inner ? 'Internal' : 'External' ) + ' building (' + e.size + 'x' + e.size + ')' + br;
    if ( e.capacity ) {
       result += "Capacity: " + e.capacity + ' ';
       result += ['soldier','scientist','technician'].filter( function(i){ return e[i]; } ).map( ns.ucfirst ).join( '/' ) + br;
    }
    result += ( e.destroyable ? 'D' : 'Non-d' ) + "estructible.";
-   if ( e.max ) result += " Max. 1 can be built.";
+   if ( e.max ) result += " Max. 1" + ( ui.comparing ? '' : ' can be built.' );
    return result;
 };
 
@@ -47,27 +52,26 @@ ns.get_general_desc = function ufoal_get_general_desc( e ) {
 };
 
 function percent( v ) { return Math.round(v*100, 3) + '%'; }
-function second( t ) { return Math.round(t/10, 3) + '&thinsp;s.'; }
+function second( t ) { return Math.round(t)/10 + '&thinsp;s.'; }
 
 ns.get_item_desc = function ufoal_get_item_desc( e ) {
-   var sub, result = [], ui = ns.ui;
+   var sub, result = [];
    function add( t ) { result.push( t ); }
 
-   if ( ! ns.ui.comparing ) result.push( ns.get_general_desc( e ) + hr );
+   if ( ! ui.comparing ) result.push( ns.get_general_desc( e ) + hr );
    if ( e.weapon || e.ammo ) {
       var sub = e.weapon || e.ammo;
       add( txt.shape[ sub.shapeIndex ] + ' ' + ( sub.originIndex ? txt.item_orig[ sub.originIndex ] : '' ) );
    }
    if ( e.weight ) add( 'Weight: ' + e.weight + ' kg' );
+   else if ( ui.comparing ) add( sp );
 
-   if ( ! ns.ui.comparing && e.startquantity ) add( 'Starts game with: ' + e.startquantity + ' pieces' );
+   if ( ! ui.comparing && e.startquantity ) add( 'Starts game with: ' + e.startquantity + ' pieces' );
 
-   if ( e.manufacturable ) {
+   if ( e.manufacturable && ! ui.comparing ) {
       sub = e.manufacturable;
-      if ( sub.assemblytime ) add( 'Assembly line: ' + sub.assemblytime + ( ns.ui.comparing ? ' days' : ' man-days to setup' ) );
-      if ( sub.manufacturingtime ) add( 'Produce: ' + sub.manufacturingtime + ( ns.ui.comparing ? ' days' : ' man-days per piece' ) );
-   } else if ( ns.ui.comparing ) {
-      add( sp ); add( sp );
+      if ( sub.assemblytime ) add( 'Assembly line: ' + sub.assemblytime + ' man-days to setup' );
+      if ( sub.manufacturingtime ) add( 'Produce: ' + sub.manufacturingtime + ' man-days per piece' );
    }
 
    if ( e.armour ) {
@@ -75,9 +79,9 @@ ns.get_item_desc = function ufoal_get_item_desc( e ) {
       add( hr );
       if ( sub.headslotIndex && sub.handslotIndex ) add( 'Addon slots: 2' );
       else if ( sub.headslotIndex || sub.handslotIndex ) add( 'Addon slot: 1' );
-      else if ( ns.ui.comparing ) add( 'No addon slot' );
+      else if ( ui.comparing ) add( 'No addon slot' );
       if ( sub.maxhostility ) add( 'Env. Resist: ' + sub.maxhostility );
-      else if ( ns.ui.comparing ) add( 'Env. Resist: 15' );
+      else if ( ui.comparing ) add( 'Env. Resist: 15' );
       if ( sub.protection ) sub.protection.forEach( function( e, i ) {
          if ( i ) add( txt.damage_type[i] + ': ' + percent( e ) );
       });
@@ -114,9 +118,9 @@ ns.get_item_desc = function ufoal_get_item_desc( e ) {
       if ( sub.visorslotIndex ) slot.push( 'Scope' );
       if ( sub.additionalslotIndex ) slot.push( 'Underbarrel' );
       if ( slot.length ) add( 'Addons: ' + slot.join( ', ' ) );
-      else if ( ns.ui.comparing ) add( 'Addons: (None)' );
+      else if ( ui.comparing ) add( 'Addons: (None)' );
       if ( sub.deploytime ) add( 'Deploy: ' + second( sub.deploytime ) );
-      else if ( ns.ui.comparing ) add( 'Deploy: 0s' );
+      else if ( ui.comparing ) add( 'Deploy: 0s' );
 
       if ( sub.ammo ) { sub.ammo.forEach( function( ammo ) {
          add( ' ' );
@@ -146,7 +150,7 @@ ns.get_ammo_desc = function ufoal_get_ammo_desc( wam ) {
          + ( wam.effectprobability < 1 ? ' ' + percent( wam.effectprobability ) + ' chance' : '' );
    }
    line += indent;
-   if ( wam.strength && wam.dmgtype ) line += ' ' + wam.strength + ' ' + wam.dmgtype + ' damage';
+   if ( wam.strength && wam.dmgtype ) line += ' ' + wam.strength + ' ' + wam.dmgtype + ( ui.comparing ? '' : ' damage' );
 
    var sub = null;
    if ( wam.ranged ) sub = wam.ranged;
@@ -155,7 +159,7 @@ ns.get_ammo_desc = function ufoal_get_ammo_desc( wam ) {
       if ( sub.range ) line += ' ' + sub.range + ' m';
       if ( sub.aimingtime ) line += ' ' + second( sub.aimingtime );
       if ( sub.accuracy && sub.accuracy < 10 ) line += ', accuracy x' + sub.accuracy;
-	  if ( sub.launched && sub.launched.animationspeed ) line += " speed " + sub.launched.animationspeed + ", ";
+      if ( sub.launched && sub.launched.animationspeed ) line += " speed " + sub.launched.animationspeed + ", ";
    }
 
    if ( wam.radius ) {
@@ -200,7 +204,7 @@ ns.get_race_desc = function ufoal_get_race_desc( e ) {
 };
 
 ns.get_subrace_desc = function ufoal_get_subrace_desc( e ) {
-   var result = '', ui = ns.ui;
+   var result = '';
    result += 'Exp award: ' + percent( e.exp ) + br + 'Size: ' + percent( e.size ) + br;
    result += 'Speed: ' + percent( e.speed ) + br + 'Capacity: ' + e.capacity + ' kg' + br;
    result += 'Stability:' + ( e.stability < 10 ? percent( e.stability ) : 'Stable' ) + br;
@@ -226,7 +230,7 @@ ns.get_subrace_desc = function ufoal_get_subrace_desc( e ) {
 };
 
 ns.get_unit_desc = function ufoal_get_unit_desc( e ) {
-   var result = '', ui = ns.ui;
+   var result = '';
    if ( e.typeIndex ) result += txt.unit_type[ e.typeIndex ] + br;
    if ( e.level ) result += 'Level: ' + e.level + br;
    if ( e.training ) {
@@ -267,7 +271,7 @@ ns.get_unit_desc = function ufoal_get_unit_desc( e ) {
 };
 
 ns.get_squad_desc = function ufoal_get_squad_desc( e ) {
-   var result = '', ui = ns.ui;
+   var result = '';
    result += e.faction + ' ' + ns.uncamel( ns.ucfirst( txt.squad_type[ e.typeIndex ] ) ) + br ;
    result += 'Strength: ' + e.strength + br;
    result += 'Members: ' + e.min_mem;
@@ -286,12 +290,20 @@ ns.get_squad_desc = function ufoal_get_squad_desc( e ) {
 };
 
 ns.get_people_desc = function ufoal_get_people_desc( e ) {
-   var result = '', ui = ns.ui;
-   if ( e.family ) result += ns.txt.family[ e.family ] + ' Family' + br + br;
-   result += 'Level' + br;
-   if ( e.soldier ) result += sp + 'Soldier ' + e.soldier + br;
-   if ( e.scientist ) result += sp + 'Scientist ' + e.scientist + br;
-   if ( e.technician ) result += sp + 'Technician ' + e.technician + br;
+   var result = '';
+   if ( e.family ) result += ns.txt.family[ e.family ] + ' Family' + br;
+   else if ( ui.comparing ) result += br;
+   result += br + 'Level' + br;
+   if ( e.soldier ) result += sp + 'Soldier ' + e.soldier + br; else if ( ui.comparing ) result += br;
+   if ( e.scientist ) result += sp + 'Scientist ' + e.scientist + br; else if ( ui.comparing ) result += br;
+   if ( e.technician ) result += sp + 'Technician ' + e.technician + br; else if ( ui.comparing ) result += br;
+   if ( e.training ) {
+      result += br + 'Training: ' +  ns.data.training.filter( function(t) { return e.training.indexOf( t.id ) >= 0 } )
+         .map( ui.create_title ).join( ', ' ) + br;
+   } else if ( ui.comparing ) {
+      result += br + sp + br;
+   }
+
    if ( e.trigger === 'StartEvent' )
       result += br + 'On the team since game starts' + br;
    else if ( e.trigger === 'UnreachableTrigger' )
@@ -299,13 +311,6 @@ ns.get_people_desc = function ufoal_get_people_desc( e ) {
    else
       result += br + 'Joins when ' + ns.uncamel( e.trigger ) + br;
 
-   if ( e.training ) {
-      for ( var i in ns.data.training ) {
-         if ( ns.data.training[ i ].id === e.training[0] ) {
-            result += br + 'Training: ' + ui.create_title( ns.data.training[ i ] ) + br;
-         }
-      }
-   }
    if ( e.attributes ) {
       result += br + 'Starting Attributes' + br;
       for ( var k in e.attributes ) {
